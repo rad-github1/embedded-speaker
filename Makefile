b2c773fe5a8bd232fd4819b377181b22ab653f42
rad-github1 <welcome@privateolive.maskmy.id>
2024-01-15 23:42:25 +0800

    LED flashes every 2 seconds from start

--- Makefile
+++ Makefile
@@ -1,1 +1,6 @@
+default:
+			avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o speaker.o speaker.c
 
+			avr-gcc -o speaker.bin speaker.o
+			avr-objcopy -O ihex -R .eeprom speaker.bin speaker.hex
+			sudo avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:speaker.hex
--- speaker.c
+++ speaker.c
@@ -1,18 +1,25 @@
+// IME:     RADNIK KOMELOM
+// DATUM:   14.I.2023
+// PORUKA:  ...
+
 #include <avr/io.h>
-#include <util.delay.h>
+#include <util/delay.h>
 
 int main() {
 
-  // SETUP
+  // SETUP FUNCTION
+  DDRB = DDRB | (1 << DDB5);
 
-
-  // LOOP
+  // LOOP FUNCTION
   while(1)
   {
-    
+    PORTB = PORTB | (1 << PORTB5);
 
+    _delay_ms(2000);
 
+    PORTB = PORTB & ~(1 << PORTB5);
+
+    _delay_ms(2000);
   }
-
   return 0;
 }
