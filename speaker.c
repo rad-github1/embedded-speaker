024d4c60378675cd14ac19ec09ca7a1470f8b843
rad-github1 <welcome@privateolive.maskmy.id>
2024-01-16 17:08:23 +0800

    Make LED on port 7 turn on and off every second

--- speaker.c
+++ speaker.c
@@ -8,18 +8,24 @@
 int main() {
 
   // SETUP FUNCTION
-  DDRB = DDRB | (1 << DDB5);
+  // DDRB = DDRB | (1 << DDB5);
+  // DDRB = 0b00100000;
+  DDRD = 0b10000000;
 
   // LOOP FUNCTION
   while(1)
   {
-    PORTB = PORTB | (1 << PORTB5);
+    // PORTB = PORTB | (1 << PORTB5);
+    // PORTB = 0b00100000;
+    PORTD = 0b10000000;
 
-    _delay_ms(2000);
+    _delay_ms(1000);
 
-    PORTB = PORTB & ~(1 << PORTB5);
+    // PORTB = PORTB & ~(1 << PORTB5);
+    // PORTB = 0b00000000;
+    PORTD = 0b00000000;
 
-    _delay_ms(2000);
+    _delay_ms(1000);
   }
   return 0;
 }
